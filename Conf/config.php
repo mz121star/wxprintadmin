5c6e41dd2fe6fc50c6933a2b8c29a518b6a58da7
mz121star <mz121star@gmail.com>
2014-08-28 17:29:04 +0800

    zaa

--- Conf/config.php
+++ Conf/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'DB_PREFIX' => 'wx_',
-    'DB_DSN' => 'mysql://root:820819@localhost:3306/wxprint',
+    'DB_DSN' => 'mysql://root:8ecba89b81@localhost:3306/wxprintadmin',
 
 
     'DEFAULT_FILTER'=>'htmlspecialchars,stripslashes',
