15abab3438012888dadf5f452337bc98e8a61ea9
mz121star <mz121star@gmail.com>
2014-09-01 00:44:42 +0800

    aa

--- Home/Lib/Action/WeixinAction.class.php
+++ Home/Lib/Action/WeixinAction.class.php
@@ -45,7 +45,7 @@ class WeixinAction extends Action {
        /* $wcHelper=new wechatHelper();
         $wcHelper->inserPic($fromuser,$picurl);*/
         $content = array();
-        $content[] = array("Title"=>"图片上传成功",  "Description"=>"图片上传成功，接下来可以打印此图片", "PicUrl"=>$object['PicUrl'], "Url" =>"http://print.wx.dlwebs.com/zoom.php?id=".$object['FromUserName']);
+        $content[] = array("Title"=>"图片上传成功",  "Description"=>"图片上传成功，接下来可以打印此图片", "PicUrl"=>"", "Url" =>"http://print.wx.dlwebs.com/zoom.php?id=".$object['FromUserName']);
 
         $result = $this->transmitNews($object, $content);
         return $result;
